a91365c0cbb857040f95369595d63ae6e62e909b
Siqi Shen <shensq@umich.edu>
2020-03-27 01:28:30 -0400

    update the script for conparison experiment

--- code/run_compare_aug.sh
+++ code/run_compare_aug.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 pwd
 
-# python retrieve_candidate.py --model_dir mi_nli
+NUM_EPOCHS=5
+NUM_TURNS=5
 
-#mkdir -p ../models/mi_tuned_5turn
-#python gpt_tuning.py --output_dir mi_tuned_5turn --num_train_epochs 10 --num_turns 5
-#python gpt_sample.py --model_dir mi_tuned_5turn --output_dir mi_tuned_5turn --num_turns 5 --top_p 0.95
-#
-#mkdir -p ../models/mi_tuned_aug
-#python gpt_tuning.py --output_dir mi_tuned_aug --num_train_epochs 10 --num_turns 5 --augment
-#python gpt_sample.py --model_dir mi_tuned_aug --output_dir mi_tuned_aug --num_turns 5 --augment  --top_p 0.95
+MODEL_PATH="kbert"
+mkdir -p ../models/${MODEL_PATH}
+python gpt_tuning.py --output_dir ${MODEL_PATH} --num_train_epochs ${NUM_EPOCHS} --num_turns ${NUM_TURNS} --kbert
+python gpt_sample.py --model_dir ${MODEL_PATH} --output_dir ${MODEL_PATH} --num_turns ${NUM_TURNS} --top_p 0.95 --kbert
 
-# mkdir -p ../models/mi_tuned_keyword
-#python gpt_tuning.py --output_dir mi_tuned_keyword --num_train_epochs 10 --num_turns 5 --keyword
-# python gpt_sample.py --model_dir mi_tuned_keyword --output_dir mi_tuned_keyword --num_turns 5 --keyword  --top_p 0.95
+MODEL_PATH="kbert_position"
+mkdir -p ../models/${MODEL_PATH}
+python gpt_tuning.py --output_dir ${MODEL_PATH} --num_train_epochs ${NUM_EPOCHS} --num_turns ${NUM_TURNS} --kbert --kbert_position
+python gpt_sample.py --model_dir ${MODEL_PATH} --output_dir ${MODEL_PATH} --num_turns ${NUM_TURNS} --top_p 0.95 --kbert --kbert_position
 
-# mkdir -p ../models/mi_tuned_both
-# python gpt_tuning.py --output_dir mi_tuned_both --num_train_epochs 10 --num_turns 10 --keyword --augment
-# python gpt_sample.py --model_dir mi_tuned_both --output_dir mi_tuned_both --num_turns 10 --keyword --augment --top_p 0.95
+MODEL_PATH="kbert_mask_position"
+mkdir -p ../models/${MODEL_PATH}
+python gpt_tuning.py --output_dir ${MODEL_PATH} --num_train_epochs ${NUM_EPOCHS} --num_turns ${NUM_TURNS} --kbert --kbert_position --kbert_mask
+python gpt_sample.py --model_dir ${MODEL_PATH} --output_dir ${MODEL_PATH} --num_turns ${NUM_TURNS} --top_p 0.95 --kbert --kbert_position --kbert_mask
 
-mkdir -p ../models/mi_tuned_kbert_mask
-python gpt_tuning.py --output_dir mi_tuned_kbert_mask --num_train_epochs 1 --num_turns 5 --kbert
-python gpt_sample.py --model_dir mi_tuned_kbert_mask --output_dir mi_tuned_kbert_mask --num_turns 5 --top_p 0.95 --kbert
-
-echo "Finished."
+#echo "Finished."
 
